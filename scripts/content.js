312411457a902b84edb49a6f98bf9d8b099709d0
chibicco <me@chibicco.dev>
2024-07-19 05:25:53 +0900

    feat: Toggle feature Enable/Disable on button click

--- scripts/content.js
+++ scripts/content.js
@@ -1,32 +1,56 @@
 const hideComments = () => {
-    const commentsSection = document.querySelector('#comments');
-    if (commentsSection) {
-      commentsSection.style.visibility = 'hidden';
-    }
-  };
+  const commentsSection = document.querySelector('#comments');
+  if (commentsSection) {
+    commentsSection.style.visibility = 'hidden';
+  }
+};
 
-  window.addEventListener('load', () => {
+const showComments = () => {
+  const commentsSection = document.querySelector('#comments');
+  if (commentsSection) {
+    commentsSection.style.visibility = 'visible';
+  }
+};
+
+const toggleComments = (hide) => {
+  if (hide) {
     hideComments();
+  } else {
+    showComments();
+  }
+};
+
+chrome.storage.sync.get(['YouTubeCommentHideEnabled'], (result) => {
+  toggleComments(result.YouTubeCommentHideEnabled);
+
+  const sectionsElement = document.querySelector('#sections');
+  if (sectionsElement) {
+    const observer = new MutationObserver(() => toggleComments(result.YouTubeCommentHideEnabled));
+    observer.observe(sectionsElement, {
+      childList: true,
+      subtree: true,
+    });
+  }
+});
+
+document.addEventListener('yt-navigate-finish', () => {
+  chrome.storage.sync.get(['YouTubeCommentHideEnabled'], (result) => {
+    toggleComments(result.YouTubeCommentHideEnabled);
 
     const sectionsElement = document.querySelector('#sections');
     if (sectionsElement) {
-      const observer = new MutationObserver(hideComments);
+      const observer = new MutationObserver(() => toggleComments(result.YouTubeCommentHideEnabled));
       observer.observe(sectionsElement, {
         childList: true,
         subtree: true,
       });
     }
   });
+});
 
-  document.addEventListener('yt-navigate-finish', () => {
-    hideComments();
-
-    const sectionsElement = document.querySelector('#sections');
-    if (sectionsElement) {
-      const observer = new MutationObserver(hideComments);
-      observer.observe(sectionsElement, {
-        childList: true,
-        subtree: true,
-      });
-    }
-  });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.YouTubeCommentHideEnabled !== undefined) {
+    toggleComments(request.YouTubeCommentHideEnabled);
+    sendResponse({ status: 'success' });
+  }
+});
